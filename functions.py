8de21eb482a08f1e841f2c3b4bf1da0a8656bc69
Matthew-Calish <maxel801@wp.pl>
2024-05-17 17:07:44 +0200

    klasem zrobilem hihi

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+from math import pi
+
 def check_float(element):
 
     '''
@@ -27,3 +29,53 @@ def check(element, conditions, type):
         while element <= conditions[0] or element > conditions[len(conditions)-1]:
             element = check_float(input('Inproper value. Please try again: '))
     return element
+
+class geometry:
+
+    def __init__(self):
+        None
+
+    def set_geometry(self, system):
+        
+        if system == '1':
+            self.ambn = check(check_float(input('\nSet the distance between electrodes [m]: ')), [0, 10], 'range')
+            self.mn = None
+            self.a_position = 0
+            self.b_position = 3*self.ambn
+            self.m_position = self.ambn
+            self.n_position = 2*self.ambn
+            self.middle = self.ambn+self.ambn/2
+            self.geometry_factor = pi*2*self.ambn
+
+        elif system == '2':
+            self.ambn = check(check_float(input('\nSet the distance between A-M and B-N electrodes [m]: ')), [0, 10], 'range')
+            self.mn = check(check_float(input('\nSet the distance between M-N electrodes [m]: ')), [0, 10], 'range')
+            self.a_position = 0
+            self.b_position = 2*self.ambn+self.mn
+            self.m_position = self.ambn
+            self.n_position = self.ambn+self.mn
+            self.middle = self.ambn+self.mn/2
+            self.geometry_factor = pi*self.ambn*(self.ambn+self.mn)/self.mn
+
+        else:
+            self.variant = check(input('\nWhat profiling variant do you want to use?\n1 -> Forward\n2 -> Backward\nSelect: '), ['1', '2'], 'choice')
+            self.ambn = check(check_float(input('\nSet the distance between A-M electrodes [m]: ')), [0, 10], 'range')
+            self.mn = check(check_float(input('\nSet the distance between M-N electrodes [m]: ')), [0, 10], 'range')
+            
+            if self.variant == '1':
+                self.a_position = 0
+                self.b_position = None
+                self.m_position = self.ambn
+                self.n_position = self.ambn+self.mn
+                self.middle = self.ambn+self.mn/2
+            else:
+                self.a_position = None
+                self.b_position = self.mn+self.ambn
+                self.m_position = 0
+                self.n_position = self.mn
+                self.middle = self.mn/2               
+
+            self.geometry_factor = pi*self.ambn*(self.ambn+self.mn)/self.mn
+            
+
+    
