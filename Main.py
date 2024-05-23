c23d2ae3f23a7ed84993e90312c05494bdcef6be
Matthew-Calish <maxel801@wp.pl>
2024-05-23 16:24:05 +0200

    koniec.1

--- Main.py
+++ Main.py
@@ -1,35 +1,26 @@
 import functions as f
-from math import pi
 from matplotlib import pyplot as plt
 
-resistance_1 = f.check(f.check_float(input('Set the electrical resistance of the first layer [Ωm]: ')), [0, 1000], 'range')
-resistance_2 = f.check(f.check_float(input('\nSet the electrical resistance of the second layer [Ωm]: ')), [0, 1000], 'range')
-current = f.check(f.check_float(input('\nSet the value of elctrical current in the supply circut [A]: ')), [0, 1000], 'range')
-#k12 = (resistance_2-resistance_1)/(resistance_1+resistance_2)
+resistance_1 = f.check(f.check_float(input('Set the electrical resistance of the first layer [Ωm]: ')), [0, 100000], 'range')
+resistance_2 = f.check(f.check_float(input('\nSet the electrical resistance of the second layer [Ωm]: ')), [0, 100000], 'range')
+current = f.check(f.check_float(input('\nSet the value of elctrical current in the supply circut [A]: ')), [0, 100000], 'range')
+
 env_data = [resistance_1, resistance_2, current]
 
 mes_system = f.check(input('\nWhat measuring system do you want to use?\n1 -> Wenner\'s System\n2 -> Schlumberger\'s System\n3 -> Three-electrode System\nSelect: '), ['1', '2', '3'], 'choice')
 
 measurement = f.measurement(mes_system)
 
-#print(measurement.a_position)
-#print(measurement.b_position)
-#print(measurement.middle)
-print(measurement.geometry_factor)
-#print(measurement.ambn)
-#print(measurement.mn)
-
 x_delta = f.check(f.check_float(input('\nSet the distance between every measurement point: ')), [0, 10], 'range')
 
+app_res_values = measurement.measure(mes_system, x_delta, env_data)
 
-y = measurement.measure(mes_system, x_delta, env_data)
-print(y)
 
 figure = plt.figure()
 ax = figure.add_subplot()
 ax.set_xticks([i for i in range(0,100) if i%2 == 0])
 #plt.plot(measurement.x_values, y)
-plt.scatter(measurement.x_values, y)
-#plt.yscale('log')
+plt.scatter(measurement.x_values, app_res_values)
+plt.yscale('log')
 plt.grid()
 plt.show()
--- functions.py
+++ functions.py
@@ -31,11 +31,13 @@ def check(element, conditions, type):
     return element
 
 
-
-
 class measurement:
 
     def __init__(self, system):
+
+        '''
+        Creates an object of the measurement class based on the chosen measurement system.
+        '''
         
         if system == '1':
             self.ambn = check(check_float(input('\nSet the distance between electrodes [m]: ')), [0, 10], 'range')
@@ -45,6 +47,8 @@ class measurement:
             self.m_position = self.ambn
             self.n_position = 2*self.ambn
             self.middle = self.ambn+self.ambn/2
+            self.distance1 = self.ambn
+            self.distance2 = 2*self.ambn
             self.geometry_factor = pi*2*self.ambn
 
         elif system == '2':
@@ -55,6 +59,8 @@ class measurement:
             self.m_position = self.ambn
             self.n_position = self.ambn+self.mn
             self.middle = self.ambn+self.mn/2
+            self.distance1 = self.ambn
+            self.distance2 = self.ambn+self.mn
             self.geometry_factor = pi*self.ambn*(self.ambn+self.mn)/self.mn
 
         else:
@@ -73,76 +79,93 @@ class measurement:
                 self.b_position = self.mn+self.ambn
                 self.m_position = 0
                 self.n_position = self.mn
-                self.middle = self.mn/2               
+                self.middle = self.mn/2
 
-            self.geometry_factor = pi*self.ambn*(self.ambn+self.mn)/self.mn
+            self.distance1 = self.ambn
+            self.distance2 = self.ambn+self.mn
+            self.geometry_factor = 2*pi*self.ambn*(self.ambn+self.mn)/self.mn
+
             
     def measure(self, system, x_delta, env_data):
 
-        def check_side(p1,p2):
-            if (p1 <= 50 and p2 <= 50):
+        '''
+        Calculates the apparent resistivity for all of the point on a 2D profile of a two layer model.
+        '''
+
+        def check_side(e, r):
+
+            '''
+            Checks if the emitter and reciever electrodes are placed on the same layer.
+            '''
+
+            if (e <= 50 and r <= 50):
                 return [True, 0, 0]
-            elif (p1 >= 50 and p2 >= 50):
+            elif (e >= 50 and r >= 50):
                 return [True, 1, 1]
-            elif (p1 < 50 and p2 > 50):
+            elif (e < 50 and r > 50):
                 return [False, 0, 1]
-            elif (p1 > 50 and p2 < 50):
+            elif (e > 50 and r < 50):
                 return [False, 1, 0]
 
             
-        def distance_prim(p1,p2):
-            if p1 < 50:
-                return abs(50+(50-p1)-p2)
+        def distance_prim(e, r):
+
+            '''
+            Calculates the distance between the reciever and the reflection point of emitter.
+            '''
+
+            if e < 50:
+                return abs(50+(50-e)-r)
             else:
-                return abs(50-(p1-50)-p2)
+                return abs(50-(e-50)-r)
             
             
         def voltage(side, res, curr, distance, distance_prim, direction):
+
+            '''
+            Calculates the voltage value in a given point.
+
+            side --> Information about the placement of the electrodes - What layer are they on? Are they on the same layer?
+
+            res --> Information about the resistivity values of both of the layers.
+
+            curr --> The electrical current value in the supply circut.
+
+            distance --> Distance between emitter and reciever.
+
+            distance_prim --> Distance between the reciever and the reflection point of emitter.
+
+            direction --> Information about the direction of the measurement. 1 if electrode A is the reference point or -1 if electrode B is the reference point.
+            '''
 
             if side[1] == 0:
                 k12 = (res[1]-res[0])/(res[0]+res[1])
             else:
                 k12 = (res[0]-res[1])/(res[0]+res[1])
 
-            print(side)
-
             if side[0] == True:
-                print(f'{distance}  {distance_prim}')
-                v = direction*(curr*res[side[2]]/(2*pi*distance)+k12*curr*res[side[2]]/(2*pi*distance_prim))
-            else:
-                v = (direction*curr*res[side[2]]/(2*pi*distance))*(1-k12)
-            return v
+                return direction*(curr*res[side[2]]/(2*pi*distance)+k12*curr*res[side[2]]/(2*pi*distance_prim))
+            
+            return (direction*curr*res[side[2]]/(2*pi*distance))*(1-k12)
+
         
-        result = []
-        x = []
+        result, x = [], []
         if system in ['1', '2']:
 
-            if system == '1':
-                distance1 = self.ambn
-                distance2 = 2*self.ambn
-            else:
-                distance1 = self.ambn
-                distance2 = self.ambn+self.mn
-
             while self.b_position <= 100:
-                print(f'dla punkt {self.middle}')
-                print(f'{self.a_position}   {self.m_position}   {self.n_position}   {self.b_position}')
-                vam = voltage(check_side(self.a_position, self.m_position), [env_data[0], env_data[1]], env_data[2],  distance1, distance_prim(self.a_position, self.m_position), 1)
-                vbm = voltage(check_side(self.b_position, self.m_position), [env_data[0], env_data[1]], env_data[2],  distance2, distance_prim(self.b_position, self.m_position), -1)
+                vam = voltage(check_side(self.a_position, self.m_position), [env_data[0], env_data[1]], env_data[2],  self.distance1, distance_prim(self.a_position, self.m_position), 1)
+                vbm = voltage(check_side(self.b_position, self.m_position), [env_data[0], env_data[1]], env_data[2],  self.distance2, distance_prim(self.b_position, self.m_position), -1)
 
-                van = voltage(check_side(self.a_position, self.n_position), [env_data[0], env_data[1]], env_data[2],  distance2, distance_prim(self.a_position, self.n_position), 1)
-                vbn = voltage(check_side(self.b_position, self.n_position), [env_data[0], env_data[1]], env_data[2],  distance1, distance_prim(self.b_position, self.n_position), -1)
-
+                van = voltage(check_side(self.a_position, self.n_position), [env_data[0], env_data[1]], env_data[2],  self.distance2, distance_prim(self.a_position, self.n_position), 1)
+                vbn = voltage(check_side(self.b_position, self.n_position), [env_data[0], env_data[1]], env_data[2],  self.distance1, distance_prim(self.b_position, self.n_position), -1)
 
                 V_delta = (vam+vbm)-(van+vbn)
                 
-
                 res_a = self.geometry_factor*V_delta/env_data[2]
-                result.append(res_a)
-                print(f'{vam}   {vbm}   {van}   {vbn}')
-                print(f'{res_a}\n')
 
+                result.append(res_a)
                 x.append(self.middle)
+
                 self.middle += x_delta
                 self.a_position += x_delta
                 self.b_position += x_delta
@@ -150,15 +173,14 @@ class measurement:
                 self.n_position += x_delta
 
         else:
-            distance1 = self.ambn
-            distance2 = self.ambn+self.mn
 
             if self.variant == '1':
                 while self.n_position <= 100:
-                    vam = voltage(check_side(self.a_position, self.m_position), [env_data[0], env_data[1]], env_data[2],  distance1, distance_prim(self.a_position, self.m_position), 1)
-                    van = voltage(check_side(self.a_position, self.n_position), [env_data[0], env_data[1]], env_data[2],  distance2, distance_prim(self.a_position, self.n_position), 1)
+                    vam = voltage(check_side(self.a_position, self.m_position), [env_data[0], env_data[1]], env_data[2],  self.distance1, distance_prim(self.a_position, self.m_position), 1)
+                    van = voltage(check_side(self.a_position, self.n_position), [env_data[0], env_data[1]], env_data[2],  self.distance2, distance_prim(self.a_position, self.n_position), 1)
 
                     V_delta = vam-van
+
                     res_a = self.geometry_factor*V_delta/env_data[2]
 
                     result.append(res_a)
@@ -166,6 +188,22 @@ class measurement:
 
                     self.middle += x_delta
                     self.a_position += x_delta
+                    self.m_position += x_delta
+                    self.n_position += x_delta
+            else:
+                while self.b_position <= 100:
+                    vbm = voltage(check_side(self.b_position, self.m_position), [env_data[0], env_data[1]], env_data[2],  self.distance2, distance_prim(self.b_position, self.m_position), -1)
+                    vbn = voltage(check_side(self.b_position, self.n_position), [env_data[0], env_data[1]], env_data[2],  self.distance1, distance_prim(self.b_position, self.n_position), -1)
+
+                    V_delta = vbm-vbn
+
+                    res_a = self.geometry_factor*V_delta/env_data[2]
+
+                    result.append(res_a)
+                    x.append(self.middle)
+
+                    self.middle += x_delta
+                    self.b_position += x_delta
                     self.m_position += x_delta
                     self.n_position += x_delta
 
